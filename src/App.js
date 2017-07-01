288ede90ebf1354b9b414c408213fd2e9ca2fe77
jlaguna <elagunaj@gmail.com>
2017-06-30 21:33:25 -0700

    create data flow for queries from weatherQuery to main app

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import WeatherResults from './WeatherResults.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.handleCityQuery = this.handleCityQuery.bind(this);
+  }
+
+  handleCityQuery(city) {
+    console.log(`${city}`);
+  }
+
   render() {
     return (
       <div>
-        <WeatherQuery />
+        <WeatherQuery handleCityQuery = {this.handleCityQuery}/>
         <WeatherResults />
       </div>
     );
--- src/WeatherQuery.js
+++ src/WeatherQuery.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 class WeatherQuery extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handleCityQuery(this.state.value);
+  }
+
   render() {
     return (
       <div>
-        <h1>Inside Weather Query!</h1>
+        <form onSubmit = {this.handleSubmit}>
+          <input type='text' value={this.state.value} onChange={this.handleChange} placeholder='Enter city'></input>
+        </form>
       </div>
     );
   }
