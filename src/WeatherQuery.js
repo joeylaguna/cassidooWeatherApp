bd8a8c9a7278b795b2f3d2c66d790c2e3b537324
jlaguna <elagunaj@gmail.com>
2017-06-30 21:23:33 -0700

    Create basic component structure

---
+++ src/WeatherQuery.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WeatherQuery extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Inside Weather Query!</h1>
+      </div>
+    );
+  }
+}
+
+export default WeatherQuery;
---
+++ src/WeatherResults.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WeatherResults extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Inside Weather Results!</h1>
+      </div>
+    );
+  }
+}
+
+export default WeatherResults;
